2ac7d03f4de1af19b3d39987d7d767ba31eff3c7
Darlan de Castro <darlandc@gmail.com>
2021-03-02 01:38:34 -0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [state, setState] = useState({ data: [] });
+
+  useEffect(() => {
+    axios.get("https://api.npms.io/v2/search?q=react").then(response =>
+      setState({
+        data: response.data.results
+      })
+    );
+  }, []);
+
+  console.log(state.data);
+
+  if (state.data.length > 0) {
+    return (
+      <div className="App">
+        <header></header>
+        <main>
+          <ul>
+            {state.data.map((item) => (
+						<li key={item.package.date}>{item.package.name}</li>
+					))}
+          </ul>
+        </main>
+        <footer></footer>
+      </div>
+    );
+  } else {
+    return <h5>Loading...</h5>;
+  }
 }
 
 export default App;
